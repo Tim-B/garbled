2304c4ee29c776e30e04df0d4140d0a42012a567
Tim Bell <tim@bellfamily.com.au>
2014-03-27 16:18:30 +1000

    Adding scrolling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,26 @@
 'use strict';
 
 angular.module('garbledApp')
-    .controller('MainCtrl', function ($scope, Chatservice) {
+    .controller('MainCtrl', function ($scope, config, Chatservice, $timeout) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
-        $scope.user = "Fred Frog " + Math.round(Math.random()*101);
+        $scope.user = "Fred Frog " + Math.round(Math.random() * 101);
         $scope.newMessage = "";
         $scope.messages = Chatservice;
 
-        $scope.submitMessage = function() {
+        config.fb_ref.on('child_added', function (childSnapshot, prevChildName) {
+            $timeout(function () {
+                var objDiv = document.getElementById("message-container");
+                objDiv.scrollTop = objDiv.scrollHeight + 60;
+                console.log("New");
+            });
+        });
+
+        $scope.submitMessage = function () {
             $scope.messages.$add({from: $scope.user, message: $scope.newMessage});
             $scope.newMessage = "";
         }
